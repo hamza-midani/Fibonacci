48f2a4970417e4105981197f86e6d6d658b7f6f5
unknown <hamzamidani26@gmail.com>
2022-11-01 10:31:18 +0100

    initial code

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-let numbrOfTerme = -1;
+let numbrOfTerme = -1; //  numbreofterme  recoit un nombre negatif -1 pour entrer dans le boucle while
 while (numbrOfTerme <= 0) {
-  numbrOfTerme = parseInt(prompt("Enter the number of fibonaci sequence : "));
+  numbrOfTerme = parseInt(prompt("Enter the number of fibonaci sequence : ")); //boucle while pour numbrofterme soit positif
 }
 console.log("A fibonacci sequence is written as:");
 fibonaciSequence = (numbrOfTerme) => {
   let arr = [0, 1];
 
-  let Fn = 0;
-  let Sn = 1;
-  let S = 0;
+  let Fn = 0; //le premier nombre recoit 0
+  let Sn = 1; //le deuxieme nombre recoit 1
+  let S = 0; //somme  recoit 0
   for (let i = 1; i <= numbrOfTerme; i++) {
-    S = Fn + Sn;
-    Fn = Sn;
-    Sn = S;
-    arr.push(S);
-    console.log(`step ${i}:${arr}`);
+    S = Fn + Sn; //somme egale le premier nombre et le seconde nombre
+    Fn = Sn; //premier nombre egale seconde nombre
+    Sn = S; //seconde nombre egale somme
+    arr.push(S); //insertion la somme dans la fin array par  push
+    console.log(`step ${i}:${arr}`); //afficher chaque etape d'insertion d un valeur
   }
   console.log(`${arr}`);
   console.log(arr.join(","));
